598446d44c7fba9ff2095b82f791c26de5e08ec2
komodor8 <k.modor@yahoo.com>
2025-05-16 12:23:29 +0000

    I removed gitpod.yml and improved setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,15 +1,25 @@
-set -e
+#!/bin/bash
+set -ex
 
-echo "Devcontainer postCreateCommand exécuté !"
+echo "Devcontainer postCreateCommand executed !"
 
 # Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -o awscliv2.zip
 sudo ./aws/install
 
 # Create folder AWS config
 mkdir -p ~/.aws
 
-# Create file credentials with environment variables
-echo -e "[default]\naws_access_key_id=$AWS_ACCESS_KEY_ID\naws_secret_access_key=$AWS_SECRET_ACCESS_KEY" > ~/.aws/credentials
-echo -e "[default]\nregion=eu-west-3" > ~/.aws/config
+# Create file credentials with environment variables from github secrets
+cat > ~/.aws/credentials << EOF
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+EOF
+
+cat > ~/.aws/config << EOF
+[default]
+region = eu-west-3
+output = json
+EOF
